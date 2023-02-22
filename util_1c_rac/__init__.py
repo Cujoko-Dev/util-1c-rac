8b97ee5ee40eab5fb375de8e2ae0352e0b9784a4
Cujoko <cujoko@gmail.com>
2023-02-23 07:08:00 +0900

    Удалит всё непонятное или пока не нужное

--- util_1c_rac/__init__.py
+++ util_1c_rac/__init__.py
@@ -1,12 +1,8 @@
-"""Top-level package for tools-1c."""
+"""Top-level package for util-1c-rac"""
 from __future__ import absolute_import
 
 from .rac import Cluster
 
 
 class Cluster(Cluster):
-    __doc__ = Cluster.__doc__
-
-__author__ = """George Kvelidze"""
-__email__ = 'georgiykvelidze@gmail.com'
-__version__ = '0.1.1'
+    pass
