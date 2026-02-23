007edc2ad29c460c77daecbab988ae3a023b8c17
Cujoko <cujoko@gmail.com>
2026-02-23 16:00:12 +0900

    Ещё улучшит (?) конструктор Cluster

--- src/util_1c_rac/rac.py
+++ src/util_1c_rac/rac.py
@@ -2,10 +2,9 @@ import platform
 import time
 from pathlib import Path
 
-from commons_1c import platform_
 from loguru import logger
 
-from . import utils
+from . import base
 
 
 class InfobaseNotFound(Exception):
@@ -19,50 +18,26 @@ class Cluster:
         port: int = 1541,
         ras_host: str = platform.node(),
         ras_port: int = 1545,
-        rac_file_fullpath: Path | None = None,
-        cluster_name: str = "",
-        all_settings: dict | None = None,
+        *,
+        rac_file_fullpath: Path,
     ):
         """Class to manage cluster infobase and cluster settings.
         Args:
-            - host         (str)       -- cluster host
-            - port         (int)       -- cluster port
-            - ras_host     (str)       -- RAS host
-            - ras_port     (int)       -- RAS port
-            - rac_file_fullpath (Path) -- full path to rac.exe file
-            - cluster_name (str)       -- cluster name
-            - all_settings (dict)      -- dict with data from settings file
+            - host              (str)  -- Cluster host
+            - port              (int)  -- Cluster port
+            - ras_host          (str)  -- RAS host
+            - ras_port          (int)  -- RAS port
+            - rac_file_fullpath (Path) -- Full path to rac.exe file
         """
-        if host and port:
-            self.host = host
-            self.port = port
+        self.host = host
+        self.port = port
 
-            self.ras_host = ras_host
-            self.ras_port = ras_port
+        self.ras_host = ras_host
+        self.ras_port = ras_port
 
-            if rac_file_fullpath is None:
-                self.rac_file_fullpath = platform_.get_last_rac_exe_file_fullpath()
-            else:
-                self.rac_file_fullpath = rac_file_fullpath
-        elif cluster_name and all_settings:
-            self.all_settings = all_settings
-
-            settings = all_settings["variables"]["CLUSTERS"][cluster_name.upper()]
-
-            self.ras_host = settings["host"]
-            self.ras_port = settings["ras_port"]
-
-            self.rac_file_fullpath = Path(
-                settings["path"], settings["version"], "bin", "rac.exe"
-            )
-
-            self.sql_server = settings["default_sql_server"]
-        else:
-            raise AttributeError  # todo
+        self.rac_file_fullpath = rac_file_fullpath
 
         self._cluster_id = None
-        self.host = None
-        self.port = None
 
         output = self._run_command("cluster list", "get list of clusters")
         output_processed = self._process_output(output)
@@ -70,19 +45,14 @@ class Cluster:
         if len(output_processed) > 0:
             output_processed_item = None
 
-            if self.host and self.port:
-                for output_processed_item in output_processed:
-                    if output_processed_item[
-                        "host"
-                    ] == self.host and output_processed_item["port"] == str(self.port):
-                        break
-            else:
-                output_processed_item = output_processed[0]
+            for output_processed_item in output_processed:
+                if output_processed_item["host"] == self.host and output_processed_item[
+                    "port"
+                ] == str(self.port):
+                    break
 
             if output_processed_item:
                 self._cluster_id = output_processed_item["cluster"]
-                self.host = output_processed_item["host"]
-                self.port = output_processed_item["port"]
 
         self.infobases = self._get_list_of_infobases()
 
@@ -92,12 +62,12 @@ class Cluster:
             command,
         ]
 
-        output = utils.run_command(" ".join(commands), desc)
+        output = base.run_command(" ".join(commands), desc)
 
         return output
 
     @staticmethod
-    def _process_output(output, separator=""):
+    def _process_output(output):
         objs = []
 
         obj = None
@@ -134,14 +104,14 @@ class Cluster:
 
     def infobase_exists(self, ib_name):
         try:
-            ib_id = utils.get_value_by_key_in_dicts_list(ib_name, self.infobases)
+            ib_id = base.get_value_by_key_in_dicts_list(ib_name, self.infobases)
         except Exception:
             return False
         return True
 
     def _get_infobase_id(self, ib_name):
         try:
-            ib_id = utils.get_value_by_key_in_dicts_list(ib_name, self.infobases)
+            ib_id = base.get_value_by_key_in_dicts_list(ib_name, self.infobases)
         except Exception:
             raise InfobaseNotFound(f'Could not find the infobase: "{ib_name}"')
         return ib_id
@@ -161,54 +131,54 @@ class Cluster:
             command += f" --infobase-user={username} --infobase-pwd={pwd}"
         return command
 
-    def create_infobase(self, ib_name, sql_server=""):
-        """Create new infobase in cluster.
-        Args:
-            ib_name (str): infobase name
-            sql_server (str): name of SQL server from settings file
-        """
-        settings = self.all_settings["variables"]["SQL_SERVERS"]
+    # def create_infobase(self, ib_name, sql_server=""):
+    #     """Create new infobase in cluster.
+    #     Args:
+    #         ib_name (str): infobase name
+    #         sql_server (str): name of SQL server from settings file
+    #     """
+    #     settings = self.all_settings["variables"]["SQL_SERVERS"]
 
-        if sql_server:
-            db = settings[sql_server]
-        else:
-            db = settings[self.sql_server]
+    #     if sql_server:
+    #         db = settings[sql_server]
+    #     else:
+    #         db = settings[self.sql_server]
 
-        command = (
-            f"infobase --cluster={self._cluster_id}"
-            f" create --create-database --name={ib_name}"
-            f' --dbms={db["DBMS"]} --db-server={db["DB_HOST"]}'
-            f' --db-user={db["DB_USER"]} --db-pwd={db["DB_PASSWORD"]}'
-            f" --db-name={ib_name} --date-offset=2000 --security-level=1"
-            f" --license-distribution=allow --locale=ru"
-        )
+    #     command = (
+    #         f"infobase --cluster={self._cluster_id}"
+    #         f" create --create-database --name={ib_name}"
+    #         f' --dbms={db["DBMS"]} --db-server={db["DB_HOST"]}'
+    #         f' --db-user={db["DB_USER"]} --db-pwd={db["DB_PASSWORD"]}'
+    #         f" --db-name={ib_name} --date-offset=2000 --security-level=1"
+    #         f" --license-distribution=allow --locale=ru"
+    #     )
 
-        output = self._run_command(command, f'create infobase "{ib_name}"')
-        infobase_id = self._process_output(output)[0]["infobase"]
+    #     output = self._run_command(command, f'create infobase "{ib_name}"')
+    #     infobase_id = self._process_output(output)[0]["infobase"]
 
-        return infobase_id
+    #     return infobase_id
 
-    def drop_infobase(self, ib_name, username, pwd, mode=""):
-        """Drop infobase.
-        Args:
-            - ib_name  (str) -- infobase name
-            - username (str) -- infobase administrator username
-            - pwd      (str) -- infobase administrator password
-            - mode     (str) -- mode of deleting database
-                available value:
-                    clear-database -- clear database
-                    drop-database  -- delete database
-        """
-        infobase_id = self._get_infobase_id(ib_name)
-        command = f"infobase --cluster={self._cluster_id} drop --infobase={infobase_id}"
+    # def drop_infobase(self, ib_name, username, pwd, mode=""):
+    #     """Drop infobase.
+    #     Args:
+    #         - ib_name  (str) -- infobase name
+    #         - username (str) -- infobase administrator username
+    #         - pwd      (str) -- infobase administrator password
+    #         - mode     (str) -- mode of deleting database
+    #             available value:
+    #                 clear-database -- clear database
+    #                 drop-database  -- delete database
+    #     """
+    #     infobase_id = self._get_infobase_id(ib_name)
+    #     command = f"infobase --cluster={self._cluster_id} drop --infobase={infobase_id}"
 
-        if mode:
-            self._check_value(mode, ["clear-database", "drop-database"])
-            command += f"--{mode}"
+    #     if mode:
+    #         self._check_value(mode, ["clear-database", "drop-database"])
+    #         command += f"--{mode}"
 
-        command = self._add_user_credentials(command, username, pwd)
+    #     command = self._add_user_credentials(command, username, pwd)
 
-        self._run_command(command, f'drop infobase "{ib_name}"')
+    #     self._run_command(command, f'drop infobase "{ib_name}"')
 
     def _set_option(self, ib_name, option, mode, username="", pwd=""):
         ib_id = self._get_infobase_id(ib_name)
